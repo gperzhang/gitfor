c0af8ad4e58c76786246b1475f02c6f204111d45
阿菜 <acai@byai.com>
2020-07-29 23:26:34 +0800

    master 增加controller 注解

--- order/src/main/java/com/mall/order/controller/HelloController.java
+++ order/src/main/java/com/mall/order/controller/HelloController.java
@@ -1,8 +1,17 @@
 package com.mall.order.controller;
 
+import com.mall.order.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 /**
  * @author acai
  * @date 2020/7/26
  **/
+@Controller
 public class HelloController {
+    @Autowired
+    private HelloService helloService;
+
+
 }
--- order/src/main/java/com/mall/order/service/HelloService.java
+++ order/src/main/java/com/mall/order/service/HelloService.java
@@ -1,9 +1,12 @@
 package com.mall.order.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author acai
  * @date 2020/7/26
  **/
+@Service
 public interface HelloService {
     String sayHello(Long status,String title,int age,Boolean flag);
 
