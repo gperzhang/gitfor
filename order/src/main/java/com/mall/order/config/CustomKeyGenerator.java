7306fa196344a9d8ea0cc9a513894e451c43d7fa
阿菜 <acai@byai.com>
2020-10-28 22:04:45 +0800

    keyGenerator

---
+++ order/src/main/java/com/mall/order/config/CustomKeyGenerator.java
@@ -0,0 +1,69 @@
+package com.mall.order.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author acai
+ * @date 2020/10/28
+ **/
+@Configuration
+public class CustomKeyGenerator {
+
+    @Bean
+    public KeyGenerator myKeyGenerator(){
+        return ((target,method,params)->{
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName()).append(".");
+            sb.append(method.getName()).append(".");
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            System.out.println("keyGenerator=" + sb.toString());
+            return sb.toString();
+        });
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory lettuceConnectionFactory) {
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        // 设置缓存管理器管理的缓存的默认过期时间
+        defaultCacheConfig = defaultCacheConfig.entryTtl(Duration.ofSeconds(200))
+                // 设置 key为string序列化
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                // 设置value为json序列化
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                // 不缓存空值
+                .disableCachingNullValues();
+
+        Set<String> cacheNames = new HashSet<>();
+        cacheNames.add("test");
+
+        // 对每个缓存空间应用不同的配置
+        Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("test", defaultCacheConfig.entryTtl(Duration.ofSeconds(200)));
+
+        RedisCacheManager cacheManager = RedisCacheManager.builder(lettuceConnectionFactory)
+                .cacheDefaults(defaultCacheConfig)
+                .initialCacheNames(cacheNames)
+                .withInitialCacheConfigurations(configMap)
+                .build();
+        return cacheManager;
+    }
+
+
+}
